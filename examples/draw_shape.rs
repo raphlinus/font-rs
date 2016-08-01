c96b0d4b1838546c7c476900718859b11af634ee
Raph Levien <raph@google.com>
2016-08-01 10:23:37 -0700

    Add examples
    Add "draw_shape_ and "render" examples, which are useful for
    exercising the library.

---
+++ examples/draw_shape.rs
@@ -0,0 +1,38 @@
+// Copyright 2016 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A simple test program for exercising the rasterizer.
+
+extern crate font_rs;
+
+use std::io::{stdout, Write};
+
+use font_rs::raster::Raster;
+use font_rs::geom::Point;
+
+fn draw_shape(r: &mut Raster, s: f32) {
+    r.draw_line(&Point{x:s*10.0, y:s*10.5}, &Point{x: s*20.0, y: s*150.0});
+    r.draw_line(&Point{x:s*20.0, y:s*150.0}, &Point{x: s*50.0, y: s*139.0});
+    r.draw_quad(&Point{x:s*50.0, y:s*139.0}, &Point{x: s*100.0, y: s*60.0}, &Point{x: s*10.0, y: s*10.5});
+}
+
+fn main() {
+    let w = 400;
+    let h = 400;
+    let mut r = Raster::new(w, h);
+    draw_shape(&mut r, 4.0);
+    let mut o = stdout();
+    let _ = o.write(format!("P5\n{} {}\n255\n", w, h).as_bytes());
+    let _ = o.write(&r.get_bitmap());
+}
---
+++ examples/render.rs
@@ -0,0 +1,67 @@
+// Copyright 2016 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+extern crate font_rs;
+
+use std::fs::File;
+use std::io::{Read, Write};
+use std::time::SystemTime;
+
+use font_rs::font::{parse, GlyphBitmap};
+
+fn dump_pgm(glyph: &GlyphBitmap, out_filename: &str) {
+    let mut o = File::create(&out_filename).unwrap();
+    let _ = o.write(format!("P5\n{} {}\n255\n", glyph.width, glyph.height).as_bytes());
+    println!("data len = {}", glyph.data.len());
+    let _ = o.write(&glyph.data);
+}
+
+fn main() {
+    let mut args = std::env::args();
+    let _ = args.next();
+    let filename = args.next().unwrap();
+    let glyph_id: u16 = args.next().unwrap().parse().unwrap();
+    let out_filename = args.next().unwrap();
+    let mut f = File::open(&filename).unwrap();
+    let mut data = Vec::new();
+    match f.read_to_end(&mut data) {
+        Err(e) => println!("failed to read {}, {}", filename, e),
+        Ok(_) => match parse(&data) {
+            Ok(font) => {
+                if out_filename == "__bench__" {
+                    for size in 1..201 {
+                        let start = SystemTime::now();
+                        let n_iter = 1000;
+                        for _ in 0..n_iter {
+                            match font.render_glyph(glyph_id, size) {
+                                Some(_glyph) => (),
+                                None => ()
+                            }
+                        }
+                        let elapsed = start.elapsed().unwrap();
+                        let elapsed = elapsed.as_secs() as f64 + 1e-9 * (elapsed.subsec_nanos() as f64);
+                        println!("{} {}", size, elapsed * (1e6 / n_iter as f64));
+                    }
+                } else {
+                    match font.render_glyph(glyph_id, 400) {
+                        Some(glyph) => dump_pgm(&glyph, &out_filename),
+                        None => println!("failed to render {} {}", filename, glyph_id)
+                    }
+                }
+            },
+            Err(_) => println!("failed to parse {}", filename)
+        }
+    }
+
+}
