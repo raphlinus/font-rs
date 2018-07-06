6e575262e4eb02bfc1765df255a101381b047478
Rob Tsuk <robtsuk@google.com>
2018-07-06 12:35:56 -0700

    Only build libaccumulate.a for x86
    It fails to build for ARM and doesn’t appear to do anything
    there anyway.

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
+#[cfg(target_arch = "x86")]
 extern crate gcc;
 
 fn main() {
+    #[cfg(target_arch = "x86")]
     gcc::Build::new()
         .file("src/accumulate.c")
         .flag("-march=native")
